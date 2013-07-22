2e2d8955c630c63540dba6df0006d9cce81368b2
Linda <lin.m.gonzalez@gmail.com>
2013-07-22 15:10:11 -0400

    update defaults
    add initialize and render fn in the view

--- js/main.js
+++ js/main.js
@@ -1,12 +1,24 @@
 var Person = Backbone.Model.extend({
   defaults: {
-    name: "John Doe",
+    name: "Homer Simpson",
     age: 40,
-    occupation: 'circus performer'
+    occupation: 'Power Plant Worker'
   }
+
 });
 
 var PersonView = Backbone.View.extend({
   tagName: 'li',
-  className: 'Person'
+
+  initialize: function() {
+    console.log(this.model);
+  },
+
+  render: function() {
+    this.$el.html(this.model.get('name') + ' - ' +this.model.get('age') +' - ' +this.model.get('occupation'));
+  }
+
 });
+
+var person = new Person;
+var personView = new PersonView({model: person});
